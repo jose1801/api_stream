79e3113d24029d83b6532f6193b798c0f8b1a491
jose1801 <jdaniel.salinas03@gmail.com>
2026-07-03 23:07:17 -0500

    Feat: Configuración de producción para Aiven y Render lista

--- src/config.js
+++ src/config.js
@@ -8,7 +8,6 @@ export const DB_PASSWORD = process.env.DB_PASSWORD || 'Daniel_2004';
 
 // 🌟 Aquí usamos DB_NAME que es la que creamos en Render, o tu base local si no existe
 export const DB_DATABASE = process.env.DB_NAME || 'stream_cipher_db';
-
 export const DB_PORT = process.env.DB_PORT || 3306;
 export const PORT = process.env.PORT || 3000;
 export const JWT_SECRET = process.env.JWT_SECRET || 'supersecreto123';
